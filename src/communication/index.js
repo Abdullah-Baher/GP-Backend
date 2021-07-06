1115f16870ef714157a0e254313c2d7497f2de76
Salah Mostafa <33065229+ZulaMostafa@users.noreply.github.com>
2021-07-07 00:08:03 +0200

    fixed communication bugs

--- src/communication/index.js
+++ src/communication/index.js
@@ -9,50 +9,52 @@ const io = require('socket.io')(server,{
 }
 })
 
+const connectedUsers = {
+  3: [],
+  4: []
+}
 
 app.use(cors())
 io.on('connection', socket => {
 
   console.log("xxx");
 
-  socket.on('join-room', (roomId, userId) => {
-    console.log("x: " + roomId + " " + userId);
+  socket.on('join-room', (roomId, userId, user, photo) => {
+
+    connectedUsers[roomId].map(x => { 
+      console.log("X: " + roomId);
+      var userId = x.userId
+      var user = x.user
+      var photo = x.photo
+      socket.emit('connection', ({userId, user, photo}))
+    })
+
+    connectedUsers[roomId].push({ 
+      userId: userId,
+      user: user,
+      photo: photo
+    })
 
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.to(roomId).broadcast.emit('user-connected', ({userId, user, photo}))
     //io.emit('user-connected', userId);
 
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      connectedUsers[roomId].splice(connectedUsers[roomId].indexOf(x => x.userId == userId));
     })
   })
 
   socket.on('user-disconnected', (roomId, userId) => { 
     console.log("test: " + userId);
     socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    for (var i = 0; i < connectedUsers[roomId].length; i++) 
+      if (connectedUsers[roomId][i].userId == userId)
+        connectedUsers[roomId].splice(i, 1);
   }) 
 
-  socket.on('OffsetChanged', ({startOffset, endOffset, role}) => {
-    io.emit('OffsetChanged', {startOffset, endOffset, role});
-  })
-
-  socket.on('CursorChanged', ({offset, role}) => {
-    io.emit('CursorChanged', {offset, role});
-  })
-
-  socket.on('Insert', ({ index, text, getrole }) => {
-    console.log(index + ' ' + text + " " + getrole);
-    io.emit('Insert', { index, text, getrole })
-  })
-
-
-  socket.on('Replace', ({ index, length, text, getrole }) => {
-    io.emit('Replace', { index, length, text, getrole })
-  })
-
-  socket.on('Delete', ({ index, length, getrole }) => {
-    io.emit('Delete', { index, length, getrole })
-  })
+  
+  
 
   socket.on('message', ({ name, message, id, photo }) => {
     console.log(name + " " + photo) 
