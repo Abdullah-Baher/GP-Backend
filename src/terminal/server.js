1c063ee3755a30b849b677c34593a82f3abf772e
shehabkhalid <shehabkhalid1999@gmail.com>
2021-07-09 04:48:37 +0200

    adde log

--- src/terminal/server.js
+++ src/terminal/server.js
@@ -3,16 +3,17 @@ const fs = require("fs");
 const http = require("http");
 var SSHClient = require("ssh2").Client;
 var utf8 = require("utf8");
-
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 var serverPort = 3002;
 
 var server = http.createServer(app);
 
 
 
-server.listen(serverPort,()=>{console.log('Terminal Started on '+serverPort)});
+server.listen(serverPort, () => { console.log('Terminal Started on ' + serverPort) });
 
 //socket.io instantiation
 const io = require("socket.io")(server, {
@@ -24,17 +25,18 @@ const io = require("socket.io")(server, {
 
 //Socket Connection
 
-try {
-    io.on("connection", function (socket)
+try
 {
-   
-    var ssh = new SSHClient();
-    ssh.on("ready", function ()
+    io.on("connection", function (socket)
+    {
+        console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',socket)
+        var ssh = new SSHClient();
+        ssh.on("ready", function ()
         {
-            console.log(socket)
-           
+
+
             socket.emit("data", "\r\n*** SSH CONNECTION ESTABLISHED ***\r\n");
-          
+
             connected = true;
             ssh.shell(function (err, stream)
             {
@@ -48,35 +50,37 @@ try {
                     stream.write(data);
                 });
                 stream.on("data", function (d)
-                    {
-                        socket.emit("data", utf8.decode(d.toString("binary")));
-                    })
+                {
+                    socket.emit("data", utf8.decode(d.toString("binary")));
+                })
                     .on("close", function ()
                     {
                         ssh.end();
                     });
             });
         })
-        .on("close", function ()
-        { console.log('close')
-            socket.emit("data", "\r\n*** SSH CONNECTION CLOSED ***\r\n");
-        })
-        .on("error", function (err)
-        {
-            console.log(err);
-            socket.emit(
-                "data",
-                "\r\n*** SSH CONNECTION ERROR: " + err.message + " ***\r\n"
-            );
-        })
-        .connect({
-            host: "compiler",
-            port: "22", // Generally 22 but some server have diffrent port for security Reson
-            username: "root", // user name
-            password: "root" // Set password or use PrivateKey
-            // privateKey: require("fs").readFileSync("PATH OF KEY ") // <---- Uncomment this if you want to use privateKey ( Example : AWS )
-        });
-});
-} catch (error) {
+            .on("close", function ()
+            {
+                console.log('close')
+                socket.emit("data", "\r\n*** SSH CONNECTION CLOSED ***\r\n");
+            })
+            .on("error", function (err)
+            {
+                console.log(err);
+                socket.emit(
+                    "data",
+                    "\r\n*** SSH CONNECTION ERROR: " + err.message + " ***\r\n"
+                );
+            })
+            .connect({
+                host: "compiler",
+                port: "22", // Generally 22 but some server have diffrent port for security Reson
+                username: "root", // user name
+                password: "root" // Set password or use PrivateKey
+                // privateKey: require("fs").readFileSync("PATH OF KEY ") // <---- Uncomment this if you want to use privateKey ( Example : AWS )
+            });
+    });
+} catch (error)
+{
     console.log(error)
 }
